78bf298aa4ce0a90211f071692ffcd1a560ea20a
Haseeb <ah085904@gmail.com>
2026-04-25 19:22:46 +0500

    create variables to store number and the result

--- script.js
+++ script.js
@@ -4,3 +4,7 @@ const numbersBtn = document.querySelectorAll(".num-btn");
 const operatorsBtn = document.querySelectorAll(".operators");
 const equalityBtn = document.querySelector("#equality-operator");
 const decimalBtn = document.querySelector("#decimal-btn");
+
+let firstNumber = "";
+let secondNumber = "";
+let result = 0;

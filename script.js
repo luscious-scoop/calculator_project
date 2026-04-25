0f4fc3022c6e10c35740796234a652c337311fc1
Haseeb <ah085904@gmail.com>
2026-04-25 19:17:52 +0500

    create initial reference variables

---
+++ script.js
@@ -0,0 +1,6 @@
+// Reference variables
+
+const numbersBtn = document.querySelectorAll(".num-btn");
+const operatorsBtn = document.querySelectorAll(".operators");
+const equalityBtn = document.querySelector("#equality-operator");
+const decimalBtn = document.querySelector("#decimal-btn");

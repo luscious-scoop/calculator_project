fb8dd99dd1a34dd3cd03ae2a13601c3ac87e665f
Haseeb <ah085904@gmail.com>
2026-04-26 14:57:59 +0500

    basic functions for calculator

--- script.js
+++ script.js
@@ -36,3 +36,19 @@ function getSecondNumber() {
     });
   });
 }
+
+function add(num1, num2) {
+  result = num1 + num2;
+}
+
+function subtract(num1, num2) {
+  result = num1 - num2;
+}
+
+function multiply(num1, num2) {
+  result = num1 * num2;
+}
+
+function divide(num1, num2) {
+  result = num1 / num2;
+}
